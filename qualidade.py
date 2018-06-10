45fc8208b51a8a765e373a88935e2f8bad65b395
francisconilton <fniltondias@gmail.com>
2018-06-10 13:41:43 -0300

    qual a idade?

--- qualidade.py
+++ qualidade.py
@@ -4,13 +4,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Lê o banco de dados e devolve a informação pedida.')
 parser.add_argument('-nome', type=str)
 args = parser.parse_args()
-
-
 nome=args.nome
-
 conn = psycopg2.connect("dbname=postgres user=postgres password=esther")
 cur = conn.cursor()
-
 cur.execute("select idade from person where nome = '{0}'".format(nome))
 resultado = cur.fetchall()[0][0]
 print("A idade de {0} é {1}".format(nome, resultado))
