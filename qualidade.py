2215e8818b85afaf923a23d9e10491641b0156da
francisconilton <fniltondias@gmail.com>
2018-06-11 05:33:53 -0300

    Roadmap 4 - conclusão

--- qualidade.py
+++ qualidade.py
@@ -1,10 +1,11 @@
 import psycopg2
 import argparse
 
-parser = argparse.ArgumentParser(description='Lê o banco de dados e devolve a informação pedida.')
+parser = argparse.ArgumentParser(description='digite -nome nas configurações, o script busca a idade no banco de dados e retorna na tela.')
 parser.add_argument('-nome', type=str)
 args = parser.parse_args()
 nome=args.nome
+
 conn = psycopg2.connect("dbname=postgres user=postgres password=esther")
 cur = conn.cursor()
 cur.execute("select idade from person where nome = '{0}'".format(nome))
